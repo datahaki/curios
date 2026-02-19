00694ef66b6d60f62de03642be9bef72d3eaa0bb
datahaki <jan.hakenberg@gmail.com>
2026-02-19 12:19:36 +0100

    missing headers

---
+++ src/main/java/ch/alpine/curios/usr/MissingHeaders.java
@@ -0,0 +1,55 @@
+// code by jph
+package ch.alpine.curios.usr;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Optional;
+
+import ch.alpine.tensor.ext.HomeDirectory;
+import ch.alpine.tensor.ext.PathName;
+
+public enum MissingHeaders {
+  ;
+  public static void visit(Path base) throws IOException {
+    for (File file : base.toFile().listFiles()) {
+      Path path = file.toPath();
+      if (Files.isDirectory(path)) {
+        visit(path);
+      } else {
+        PathName pathName = PathName.of(file.toPath());
+        if (pathName.hasExtension("java")) {
+          try (var lines = Files.lines(path)) {
+            Optional<String> optional = lines.findFirst();
+            if (optional.isPresent()) {
+              String header = optional.orElseThrow();
+              if (!header.startsWith("/")) {
+                prepend(path);
+              }
+            } else {
+              System.err.println("FILE EMPTY " + path);
+            }
+          }
+        }
+      }
+    }
+  }
+
+  private static void prepend(Path path) throws IOException {
+    List<String> list = new LinkedList<>();
+    list.add("// code by jph");
+    try (var lines = Files.lines(path)) {
+      lines.forEach(list::add);
+    }
+    Files.write(path, list);
+    IO.println(path);
+  }
+
+  static void main() throws IOException {
+    Path path = HomeDirectory.Projects.resolve();
+    visit(path);
+  }
+}
--- src/main/java/ch/alpine/subare/book/ch05/racetrack/FVPE_RaceTrack.java
+++ src/main/java/ch/alpine/subare/book/ch05/racetrack/FVPE_RaceTrack.java
@@ -2,6 +2,7 @@
 package ch.alpine.subare.book.ch05.racetrack;
 
 import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 
 import ch.alpine.subare.api.Policy;
@@ -10,6 +11,7 @@ import ch.alpine.subare.util.DiscreteUtils;
 import ch.alpine.subare.util.DiscreteVs;
 import ch.alpine.subare.util.EquiprobablePolicy;
 import ch.alpine.subare.util.ExploringStarts;
+import ch.alpine.tensor.Throw;
 import ch.alpine.tensor.Unprotect;
 import ch.alpine.tensor.io.Import;
 import ch.alpine.tensor.sca.Round;
@@ -17,8 +19,9 @@ import ch.alpine.tensor.sca.Round;
 enum FVPE_RaceTrack {
   ;
   static void main() throws IOException {
-    Path file = Unprotect.resourcePath("/ch05/track0.png");
-    Racetrack racetrack = new Racetrack(Import.of(file), 3);
+    Path path = Unprotect.resourcePath("/ch/alpine/subare/ch05/track0.png");
+    Throw.unless(Files.exists(path));
+    Racetrack racetrack = new Racetrack(Import.of(path), 3);
     FirstVisitPolicyEvaluation fvpe = new FirstVisitPolicyEvaluation( //
         racetrack, null);
     Policy policy = EquiprobablePolicy.create(racetrack);
