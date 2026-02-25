3803326c64efb65139942aa98eff4200f12c1ba2
datahaki <jan.hakenberg@gmail.com>
2026-02-25 07:10:57 +0100

    show discovery

---
+++ src/main/java/ch/alpine/curios/ShowDiscovery.java
@@ -0,0 +1,42 @@
+// code by jph
+package ch.alpine.curios;
+
+import java.awt.GridLayout;
+import java.awt.Window;
+import java.util.List;
+import java.util.function.Supplier;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.WindowConstants;
+
+import ch.alpine.bridge.cgr.InstanceDiscovery;
+import ch.alpine.bridge.lang.FriendlyFormat;
+import ch.alpine.bridge.pro.ShowProvider;
+import ch.alpine.bridge.pro.WindowProvider;
+
+enum ShowDiscovery implements WindowProvider {
+  INSTANCE;
+
+  @Override
+  public Window getWindow() {
+    List<Supplier<ShowProvider>> list = InstanceDiscovery.of(getClass().getPackageName(), ShowProvider.class);
+    JFrame jFrame = new JFrame();
+    JPanel jPanel = new JPanel(new GridLayout(list.size(), 1));
+    for (Supplier<ShowProvider> supplier : list) {
+      ShowProvider showProvider = supplier.get();
+      JButton jButton = new JButton(FriendlyFormat.defaultTitle(showProvider.getClass()));
+      jButton.addActionListener(_ -> supplier.get().runStandalone());
+      jPanel.add(jButton);
+    }
+    jFrame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+    jFrame.setContentPane(new JScrollPane(jPanel));
+    return jFrame;
+  }
+
+  static void main() {
+    INSTANCE.runStandalone();
+  }
+}
