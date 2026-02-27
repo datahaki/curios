c938344f1e7d46136d481585e79d66d7549dd3dc
datahaki <jan.hakenberg@gmail.com>
2026-02-27 13:12:44 +0100

    ird

---
+++ src/main/java/ch/alpine/curios/man/ImageRenderDemo.java
@@ -0,0 +1,78 @@
+// code by jph
+package ch.alpine.curios.man;
+
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+
+import ch.alpine.ascony.ren.AxesRender;
+import ch.alpine.ascony.ren.ImageRender;
+import ch.alpine.ascony.win.AbstractDemo;
+import ch.alpine.bridge.gfx.GeometricLayer;
+import ch.alpine.sophus.lie.se2.Se2Matrix;
+import ch.alpine.tensor.RealScalar;
+import ch.alpine.tensor.Scalar;
+import ch.alpine.tensor.Tensor;
+import ch.alpine.tensor.Tensors;
+import ch.alpine.tensor.alg.TensorMap;
+import ch.alpine.tensor.ext.ResourceData;
+import ch.alpine.tensor.io.ImageFormat;
+import ch.alpine.tensor.opt.nd.CoordinateBoundingBox;
+import ch.alpine.tensor.red.Mean;
+import ch.alpine.tensor.sca.Clips;
+
+class ImageRenderDemo extends AbstractDemo {
+  private static final CoordinateBoundingBox COORDINATE_BOUNDING_BOX = //
+      CoordinateBoundingBox.of(Clips.interval(-0.4, 1), Clips.interval(-0.35, 0.35));
+  private static final Scalar SHIFT = RealScalar.of(1.5);
+  private final BufferedImage bufferedImage_c = ResourceData.bufferedImage("/ch/alpine/curios/man/vehicle_c.png");
+  private final BufferedImage bufferedImage;
+  private final BufferedImage grayscale_alpha;
+  private final BufferedImage grayscale;
+
+  public ImageRenderDemo() {
+    bufferedImage = bufferedImage_c;
+    {
+      Tensor tensor = ImageFormat.from(bufferedImage);
+      Tensor graysc = TensorMap.of(rgba -> Tensors.of(Mean.of(rgba.extract(0, 3)), rgba.Get(3)), tensor, 2);
+      grayscale_alpha = ImageFormat.of(graysc);
+    }
+    {
+      Tensor tensor = ImageFormat.from(bufferedImage);
+      Tensor graysc = TensorMap.of(rgba -> Mean.of(rgba.extract(0, 3)), tensor, 2);
+      grayscale = ImageFormat.of(graysc);
+    }
+  }
+
+  @Override // from RenderInterface
+  public synchronized void render(GeometricLayer geometricLayer, Graphics2D graphics) {
+    AxesRender.INSTANCE.render(geometricLayer, graphics);
+    Tensor mouse = timerFrame.geometricComponent.getMouseSe2CState();
+    {
+      geometricLayer.pushMatrix(Se2Matrix.of(mouse));
+      new ImageRender( //
+          bufferedImage, //
+          COORDINATE_BOUNDING_BOX).render(geometricLayer, graphics);
+      geometricLayer.popMatrix();
+    }
+    mouse.set(SHIFT::add, 0);
+    {
+      geometricLayer.pushMatrix(Se2Matrix.of(mouse));
+      new ImageRender( //
+          grayscale_alpha, //
+          COORDINATE_BOUNDING_BOX).render(geometricLayer, graphics);
+      geometricLayer.popMatrix();
+    }
+    mouse.set(SHIFT::add, 0);
+    {
+      geometricLayer.pushMatrix(Se2Matrix.of(mouse));
+      new ImageRender( //
+          grayscale, //
+          COORDINATE_BOUNDING_BOX).render(geometricLayer, graphics);
+      geometricLayer.popMatrix();
+    }
+  }
+
+  static void main() {
+    new ImageRenderDemo().runStandalone();
+  }
+}
