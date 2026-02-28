fe545df8ed9fc66a6db13288e3ac122c9e836c49
datahaki <jan.hakenberg@gmail.com>
2026-02-28 13:37:28 +0100

    clothoid tangent defect refactor

--- src/main/java/ch/alpine/curios/img/SpectrogramRasterDemo.java
+++ src/main/java/ch/alpine/curios/img/SpectrogramRasterDemo.java
@@ -1,34 +1,45 @@
 // code by jph
 package ch.alpine.curios.img;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
+import java.awt.Container;
 
+import ch.alpine.bridge.fig.Show;
+import ch.alpine.bridge.fig.ShowGridComponent;
+import ch.alpine.bridge.fig.Spectrogram;
+import ch.alpine.bridge.pro.ManipulateProvider;
+import ch.alpine.bridge.ref.ann.ReflectionMarker;
+import ch.alpine.tensor.RealScalar;
+import ch.alpine.tensor.Scalar;
 import ch.alpine.tensor.Tensor;
 import ch.alpine.tensor.Tensors;
-import ch.alpine.tensor.Unprotect;
 import ch.alpine.tensor.alg.Subdivide;
-import ch.alpine.tensor.ext.HomeDirectory;
-import ch.alpine.tensor.fft.SpectrogramArrays;
 import ch.alpine.tensor.img.ColorDataGradients;
-import ch.alpine.tensor.img.ImageResize;
-import ch.alpine.tensor.img.Raster;
+import ch.alpine.tensor.sca.Clip;
+import ch.alpine.tensor.sca.Clips;
 import ch.alpine.tensor.sca.ply.Polynomial;
 import ch.alpine.tensor.sca.tri.Cos;
+import ch.alpine.tensor.sca.win.WindowFunctions;
 
 /** Example from Mathematica::Spectrogram:
  * Table[Cos[ i/4 + (i/20)^2], {i, 2000}] */
-/* package */ enum SpectrogramRasterDemo {
-  ;
-  static void main() throws IOException {
-    Tensor tensor = Subdivide.of(0, 100, 2000).maps(Polynomial.of(Tensors.vector(0, 5, 1))).maps(Cos.FUNCTION);
-    Tensor spectrogram = SpectrogramArrays.FOURIER.operator().half_abs(tensor);
-    Path folder = HomeDirectory.Pictures.resolve(SpectrogramRasterDemo.class.getSimpleName());
-    Files.createDirectories(folder);
-    for (ColorDataGradients colorDataGradients : ColorDataGradients.values()) {
-      Tensor image = Raster.of(spectrogram, colorDataGradients);
-      Unprotect.Export(folder.resolve(colorDataGradients.name() + ".png"), ImageResize.nearest(image, 4));
-    }
+@ReflectionMarker
+class SpectrogramRasterDemo implements ManipulateProvider {
+  public Clip clip = Clips.interval(0, 100);
+  public Integer numel = 2000;
+  public Tensor coeffs = Tensors.vector(0, 5, 1);
+  public Scalar sampleRate = RealScalar.ONE;
+  public WindowFunctions windowFunctions = WindowFunctions.DIRICHLET;
+  public ColorDataGradients cdg = ColorDataGradients.CMYK_REVERSED;
+
+  @Override
+  public Container getContainer() {
+    Tensor tensor = Subdivide.increasing(clip, numel).maps(Polynomial.of(coeffs)).maps(Cos.FUNCTION);
+    Show show = new Show();
+    show.add(Spectrogram.of(tensor, sampleRate, windowFunctions.get(), cdg));
+    return ShowGridComponent.of(show);
+  }
+
+  static void main() {
+    new SpectrogramRasterDemo().runStandalone();
   }
 }
