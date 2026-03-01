7b56b5076d4217b33ab31581ef8450d560f7d41f
datahaki <jan.hakenberg@gmail.com>
2026-03-01 18:38:28 +0100

    curios launch pad

--- src/main/java/ch/alpine/curios/CuriosLaunchPad.java
+++ src/main/java/ch/alpine/curios/CuriosLaunchPad.java
@@ -5,6 +5,7 @@ import ch.alpine.bridge.io.FileBlock;
 import ch.alpine.bridge.io.ResourceLocator;
 import ch.alpine.bridge.pro.RunLaunchPad;
 
+/** entry point to launch miniatures */
 enum CuriosLaunchPad {
   ;
   static void main() {
