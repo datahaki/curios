1a2288aa944fa1409eaf8b93384cde59c7b22c90
datahaki <jan.hakenberg@gmail.com>
2026-03-12 21:15:44 +0100

    var func show

--- src/main/java/ch/alpine/curios/fig/VariogramFunctionsShow.java
+++ src/main/java/ch/alpine/curios/fig/VariogramFunctionsShow.java
@@ -1,32 +1,41 @@
 // code by jph
 package ch.alpine.curios.fig;
 
+import java.awt.Container;
+
 import ch.alpine.bridge.fig.Plot;
+import ch.alpine.bridge.fig.PlotOption;
 import ch.alpine.bridge.fig.Show;
-import ch.alpine.bridge.pro.ShowProvider;
+import ch.alpine.bridge.fig.ShowGridComponent;
+import ch.alpine.bridge.pro.ManipulateProvider;
+import ch.alpine.bridge.ref.ann.ReflectionMarker;
 import ch.alpine.tensor.Rational;
 import ch.alpine.tensor.RealScalar;
 import ch.alpine.tensor.Scalar;
+import ch.alpine.tensor.api.ScalarUnaryOperator;
+import ch.alpine.tensor.opt.nd.CoordinateBoundingBox;
+import ch.alpine.tensor.sca.Clip;
 import ch.alpine.tensor.sca.Clips;
 import ch.alpine.tensor.sca.var.VariogramFunctions;
 
-// TODO the loops cause overcrowding -> not a good demo
-class VariogramFunctionsShow implements ShowProvider {
+@ReflectionMarker
+class VariogramFunctionsShow implements ManipulateProvider {
+  public transient VariogramFunctions vF = VariogramFunctions.GAUSSIAN;
+
   @Override
-  public Show getShow() {
+  public Container getContainer() {
     Scalar[] params = { RealScalar.ZERO, RealScalar.of(0.1), Rational.HALF, RealScalar.ONE, RealScalar.TWO };
     Show show = new Show();
-    show.setAspectRatioOne();
-    for (VariogramFunctions variograms : VariogramFunctions.values()) {
-      show.setPlotLabel(variograms.toString());
-      for (Scalar param : params)
-        try {
-          show.add(Plot.of(variograms.of(param), Clips.interval(0.5, 2)));
-        } catch (Exception exception) {
-          // System.out.println("doesnt work: "+variograms);
-        }
-    }
-    return show;
+    Clip clipx = Clips.interval(0, 2);
+    for (Scalar param : params)
+      try {
+        ScalarUnaryOperator suo = vF.of(param);
+        show.add(Plot.of(suo, Clips.interval(0, 2), PlotOption.STRICT)).setLabel(suo.toString());
+      } catch (Exception exception) {
+        // System.out.println("doesnt work: "+variograms);
+      }
+    show.setCbb(CoordinateBoundingBox.of(clipx, Clips.interval(-0.3, 2)));
+    return ShowGridComponent.of(show);
   }
 
   static void main() {
