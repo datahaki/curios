6a4f3d46349121f59e553ec60757e0568cb4ffca
datahaki <jan.hakenberg@gmail.com>
2026-03-13 10:08:07 +0100

    vfd

--- src/main/java/ch/alpine/curios/fig/VariogramFunctionsDemo.java
+++ src/main/java/ch/alpine/curios/fig/VariogramFunctionsDemo.java
@@ -8,9 +8,10 @@ import ch.alpine.bridge.fig.PlotOption;
 import ch.alpine.bridge.fig.Show;
 import ch.alpine.bridge.fig.ShowGridComponent;
 import ch.alpine.bridge.pro.ManipulateProvider;
+import ch.alpine.bridge.ref.ann.FieldClip;
+import ch.alpine.bridge.ref.ann.FieldSlider;
 import ch.alpine.bridge.ref.ann.ReflectionMarker;
 import ch.alpine.sophis.var.VariogramFunctions;
-import ch.alpine.tensor.Rational;
 import ch.alpine.tensor.RealScalar;
 import ch.alpine.tensor.Scalar;
 import ch.alpine.tensor.api.ScalarUnaryOperator;
@@ -19,26 +20,27 @@ import ch.alpine.tensor.sca.Clip;
 import ch.alpine.tensor.sca.Clips;
 
 @ReflectionMarker
-class VariogramFunctionsShow implements ManipulateProvider {
+class VariogramFunctionsDemo implements ManipulateProvider {
   public VariogramFunctions vF = VariogramFunctions.GAUSSIAN;
+  @FieldSlider
+  @FieldClip(min = "0", max = "2")
+  public Scalar param = RealScalar.ONE;
 
   @Override
   public Container getContainer() {
-    Scalar[] params = { RealScalar.ZERO, RealScalar.of(0.1), Rational.HALF, RealScalar.ONE, RealScalar.TWO };
     Show show = new Show();
     Clip clipx = Clips.interval(0, 2);
-    for (Scalar param : params)
-      try { // TODO remove try once robust
-        ScalarUnaryOperator suo = vF.of(param);
-        show.add(Plot.of(suo, Clips.interval(0, 2), PlotOption.STRICT)).setLabel(suo.toString());
-      } catch (Exception exception) {
-        // System.out.println("doesnt work: "+variograms);
-      }
+    try {
+      ScalarUnaryOperator suo = vF.of(param);
+      show.add(Plot.of(suo, Clips.interval(0, 2), PlotOption.STRICT)).setLabel(suo.toString());
+    } catch (Exception exception) {
+      // System.out.println("doesnt work: "+variograms);
+    }
     show.setCbb(CoordinateBoundingBox.of(clipx, Clips.interval(-0.3, 2)));
     return ShowGridComponent.of(show);
   }
 
   static void main() {
-    new VariogramFunctionsShow().runStandalone();
+    new VariogramFunctionsDemo().runStandalone();
   }
 }
